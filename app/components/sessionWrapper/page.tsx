dbf77a5957d51e1d3e12678ffc0729fe83fd1be9
Chandan Kumar <ck94422@gmail.com>
2024-06-21 06:36:06 +0530

    bug inserted

--- app/components/sessionWrapper/page.tsx
+++ app/components/sessionWrapper/page.tsx
@@ -4,3 +4,5 @@ import { SessionProvider } from "next-auth/react";
 export const SessionWrapper = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
+
+//test for git from chandan branch
